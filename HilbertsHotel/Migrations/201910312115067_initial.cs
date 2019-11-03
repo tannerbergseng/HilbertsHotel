9a44222397c8b89b2a45dbb74ea8e87dc7527e4e
tanner bergseng <tannermbergseng@gmail.com>
2019-11-03 11:39:30 -0800

    Re-upload

--- HilbertsHotel/Migrations/201910312115067_initial.cs
+++ HilbertsHotel/Migrations/201910312115067_initial.cs
@@ -1,44 +1,44 @@
-namespace HilbertsHotel.Migrations
-{
-    using System;
-    using System.Data.Entity.Migrations;
+//namespace HilbertsHotel.Migrations
+//{
+//    using System;
+//    using System.Data.Entity.Migrations;
     
-    public partial class initial : DbMigration
-    {
-        public override void Up()
-        {
-            CreateTable(
-                "dbo.Patrons",
-                c => new
-                    {
-                        ID = c.Int(nullable: false, identity: true),
-                        Name = c.String(),
-                        DateOfBirth = c.DateTime(),
-                        Reservations_ID = c.String(maxLength: 128),
-                    })
-                .PrimaryKey(t => t.ID)
-                .ForeignKey("dbo.Reservations", t => t.Reservations_ID)
-                .Index(t => t.Reservations_ID);
+//    public partial class initial : DbMigration
+//    {
+//        public override void Up()
+//        {
+//            CreateTable(
+//                "dbo.Patrons",
+//                c => new
+//                    {
+//                        ID = c.Int(nullable: false, identity: true),
+//                        Name = c.String(),
+//                        DateOfBirth = c.DateTime(),
+//                        Reservations_ID = c.String(maxLength: 128),
+//                    })
+//                .PrimaryKey(t => t.ID)
+//                .ForeignKey("dbo.Reservations", t => t.Reservations_ID)
+//                .Index(t => t.Reservations_ID);
             
-            CreateTable(
-                "dbo.Reservations",
-                c => new
-                    {
-                        ID = c.String(nullable: false, maxLength: 128),
-                        CheckInId = c.Int(nullable: false),
-                        CheckIn = c.DateTime(nullable: false),
-                        Checkout = c.DateTime(nullable: false),
-                    })
-                .PrimaryKey(t => t.ID);
+//            CreateTable(
+//                "dbo.Reservations",
+//                c => new
+//                    {
+//                        ID = c.String(nullable: false, maxLength: 128),
+//                        CheckInId = c.Int(nullable: false),
+//                        CheckIn = c.DateTime(nullable: false),
+//                        Checkout = c.DateTime(nullable: false),
+//                    })
+//                .PrimaryKey(t => t.ID);
             
-        }
+//        }
         
-        public override void Down()
-        {
-            DropForeignKey("dbo.Patrons", "Reservations_ID", "dbo.Reservations");
-            DropIndex("dbo.Patrons", new[] { "Reservations_ID" });
-            DropTable("dbo.Reservations");
-            DropTable("dbo.Patrons");
-        }
-    }
-}
+//        public override void Down()
+//        {
+//            DropForeignKey("dbo.Patrons", "Reservations_ID", "dbo.Reservations");
+//            DropIndex("dbo.Patrons", new[] { "Reservations_ID" });
+//            DropTable("dbo.Reservations");
+//            DropTable("dbo.Patrons");
+//        }
+//    }
+//}
